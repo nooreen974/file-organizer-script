012effdf6ff8f2c582fea0d7cecd61b1c4b2f89e
nooreen974 <116960897+nooreen974@users.noreply.github.com>
2026-05-19 15:50:31 +0000

    Update organizer.py

--- organizer.py
+++ organizer.py
@@ -1,4 +1,13 @@
 import os
+import shutil
 folder_path = "test_folder"
 files = os.listdir(folder_path)
-print(files)
+for file in files:
+    if file.endswith(".jpg"):
+        image_folder = folder_path + "/Images"
+        if not os.path.exists(image_folder):
+            os.makedirs(image_folder)
+        source = folder_path + "/" + file
+        destination = image_folder + "/" + file
+        shutil.move(source, destination)
+        print(file + " moved to Images folder")
